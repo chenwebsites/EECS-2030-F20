24a52ab158ca84f41f0aca340b653b8ff62b9ed3
Chen <info@chenwebsites.com>
2021-01-23 10:59:01 -0500

    Assignment2-100%

---
+++ Transcript.java
@@ -0,0 +1,77 @@
+package Assignment2;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+
+
+/* PLEASE DO NOT MODIFY A SINGLE STATEMENT IN THE TEXT BELOW.
+READ THE FOLLOWING CAREFULLY AND FILL IN THE GAPS
+
+I hereby declare that all the work that was required to 
+solve the following problem including designing the algorithms
+and writing the code below, is solely my own and that I received
+no help in creating this solution and I have not discussed my solution 
+with anybody. I affirm that I have read and understood
+the Senate Policy on Academic honesty at 
+https://secretariat-policies.info.yorku.ca/policies/academic-honesty-senate-policy-on/
+and I am well aware of the seriousness of the matter and the penalties that I will face as a 
+result of committing plagiarism in this assignment.
+
+BY FILLING THE GAPS,YOU ARE SIGNING THE ABOVE STATEMENTS.
+
+Full Name:
+Student Number:
+Course Section:
+*/
+
+
+/**
+* This class generates a transcript for each student, whose information is in the text file.
+* 
+*
+*/
+
+public class Transcript {
+	private ArrayList<Object> grade = new ArrayList<Object>();
+	private File inputFile;
+	private String outputFile;
+	
+	/**
+	 * This the the constructor for Transcript class that 
+	 * initializes its instance variables and call readFie private
+	 * method to read the file and construct this.grade.
+	 * @param inFile is the name of the input file.
+	 * @param outFile is the name of the output file.
+	 */
+	public Transcript(String inFile, String outFile) {
+		inputFile = new File(inFile);	
+		outputFile = outFile;	
+		grade = new ArrayList<Object>();
+		this.readFile();
+	}// end of Transcript constructor
+
+	/** 
+	 * This method reads a text file and add each line as 
+	 * an entry of grade ArrayList.
+	 * @exception It throws FileNotFoundException if the file is not found.
+	 */
+	private void readFile() {
+		Scanner sc = null; 
+		try {
+			sc = new Scanner(inputFile);	
+			while(sc.hasNextLine()){
+				grade.add(sc.nextLine());
+	        }      
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			sc.close();
+		}		
+	} // end of readFile
+	
+
+	
+} // end of Transcript
+
